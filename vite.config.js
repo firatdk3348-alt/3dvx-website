b19c95c25d3b6c3fc0af466b0bf4872849bfea2b
Firat <firatdk3348@gmail.com>
2026-08-04 19:49:38 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        siparis: resolve(__dirname, 'siparis.html'),
+        hakkimizda: resolve(__dirname, 'hakkimizda.html')
+      }
+    }
+  }
+});
